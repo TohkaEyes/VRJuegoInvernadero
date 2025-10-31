dfc82ca9d595eb3d325f891407cfc9904277d0db
patodof <pato.dof@gmail.com>
2025-10-31 15:06:25 -0300

    Scene JUEGO y GameController
    Testingo modificar

---
+++ VRInvernaderoSeguridad/Assets/CODIGO/GameController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //TODO HACER CODIGO
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
